81d5a03e7bf9b54f3d67ded4cd44f8a3e283a8e2
vicentejrmc <vicente_pc@outlook.com>
2025-03-21 15:34:11 -0300

    Implementado Exercicio18

---
+++ Exercicio18/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio18
+{
+    internal class Program
+    {
+
+        //18. Escrever um algoritmo que gera e escreve os números ímpares entre 100 e 200.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Gerador de numeros ímpares dentro do intervalo entre 100 e 200 ---\n");
+
+            int numerosImpares = 0;
+
+            Console.WriteLine("Os numeros ímpares entre 100 e 200 são:");
+            for (int i = 100; i < 200; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    numerosImpares = i;
+                    Console.Write($"{numerosImpares}, ");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
