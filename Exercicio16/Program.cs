cac60c870d1609391cf0f5dcae105e1144080e0b
vicentejrmc <vicente_pc@outlook.com>
2025-03-21 15:16:03 -0300

    Implementado Exercicio16

---
+++ Exercicio16/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio16
+{
+    internal class Program
+    {
+        // 16. Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("---- O numereo é Par ou Impar? ----\n");
+
+                Console.Write("Digite um numero qualquer: ");
+                double numero = Convert.ToDouble(Console.ReadLine());
+
+                if (numero % 2 == 0)
+                    Console.WriteLine("O numero digitado é PAR.");
+                else
+                    Console.WriteLine("O numero digitado é IMPAR.");
+
+                Console.Write("\nVerificar ou numero? S/N": );
+                string opcao = Console.ReadLine().ToUpper();
+                if(opcao != "S")
+                    break;
+            }
+        }
+    }
+}
